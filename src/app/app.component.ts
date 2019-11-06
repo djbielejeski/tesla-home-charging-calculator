4f5d7efbe111098903763859a24365cd5d8f9f86
David B <djbielejeski@gmail.com>
2019-11-06 13:19:54 -0600

    DB: Adding in distance.  Adding in some verbage around what you are doing (distance vs time)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  kwhPrice: number = 0.09;
+  kwhPrice: number = 0.13;
   amps: number = 50;
   voltage: number = 240;
-  hours: number = 2;
-  minutes: number = 15;
+  hours: number = 5;
+  minutes: number = 0;
+  selectedOption: string = 'Time';
+  distance: number = 150;
 
-  get calculatedPrice(): number {
-    // amps * volts = watts
-    // (watts / 1000) * kwhPrice * (hours + minutes)
-    return ((this.amps * this.voltage) / 1000) * this.kwhPrice * (this.hours + (this.minutes / 60));
+  private hoursNeeded(): number {
+    let hoursNeeded: number = 0;
+    if (this.selectedOption == 'Time') {
+      hoursNeeded = (this.hours + (this.minutes / 60));
+    }
+    else {
+      let min: number = Number.parseFloat(this.minMilesPerHourChargeRate);
+      let max: number = Number.parseFloat(this.maxMilesPerHourChargeRate);
+      hoursNeeded = this.distance / ((max + min) / 2);
+    }
+
+    return hoursNeeded;
   }
 
+  get calculatedPrice(): string {
+    // amps * volts = watts
+    // (watts / 1000) * kwhPrice * (hours + minutes)
+
+
+    return '' + ((this.amps * this.voltage) / 1000) * this.kwhPrice * this.hoursNeeded();
+  }
+
+  get hoursToCharge(): string {
+    if (this.selectedOption == 'Time') {
+      return '';
+    }
+    else {
+      let hours = Math.floor(this.hoursNeeded());
+      let minutes = Math.floor(60 * (this.hoursNeeded() - hours));
+
+      return hours + "h, " + minutes + "m";
+    }
+  }
 
   get maxMilesPerHourChargeRate(): string {
     // 50 * 240 = 12000 = 34mph = 0.0028333333333333‬
@@ -37,6 +66,20 @@ export class AppComponent {
     return Number.parseFloat('' + (this.hours + (this.minutes / 60)) * Number(this.minMilesPerHourChargeRate)).toFixed(0);
   }
 
+  get outletDescription(): string {
+    if(this.amps == 80 && this.voltage == 240) {
+      return "Tesla Wall Charger"
+    }
+    else if (this.amps == 50 && this.voltage == 240) {
+      return "NEMA 14-50 Outlet"
+    }
+    else if (this.amps == 15 && this.voltage == 120) {
+      return "Standard Outlet"
+    }
+    else {
+      return "Custom Outlet (" + this.amps + " amps, " + this.voltage +" volts)";
+    }
+  }
 
   nema1450() {
     this.amps = 50;
